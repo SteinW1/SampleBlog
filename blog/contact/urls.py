ec4d3002fd6d49db47c3bb43532b49d29b1fa0ba
SteinW1 <will.stein@yahoo.com>
2021-07-21 18:59:51 -0700

    changed contact form to a class based view

--- blog/contact/urls.py
+++ blog/contact/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('contact/', views.contact, name='contact-form'),
+    #path('contact/', views.contact, name='contact-form'),
+    path('contact/', views.ContactView.as_view(), name='contact-form'),
 ]
--- blog/contact/views.py
+++ blog/contact/views.py
@@ -1,51 +1,61 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.core.mail import send_mail, BadHeaderError
+#from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse
 from django.conf import settings
+from django.views import View
 from .forms import ContactForm
 import requests # import requests module for easyier http for google recaptcha API
 
-def contact(request):
-    
-    context = {
-            'recaptcha_site_key': settings.RECAPTCHA_SITE_KEY,
-        }
-        
-    if request.method =="POST":
-        form = ContactForm(request.POST)
-        context['form'] = form # add form to context dictionary after it is declared
 
-        if form.is_valid():
+class ContactView(View):
+    
+    template = 'contact/contact.html'
+    form = ContactForm()
+    context = {
+        'recaptcha_site_key': settings.RECAPTCHA_SITE_KEY,
+    }
+        
+    def get(self, request):
+        self.context['form'] = self.form # add form to context dictionary after it is declared
+        return render(request, self.template, self.context)
+        
+    def post(self, request):
+        self.form = ContactForm(request.POST)
+        self.context['form'] = self.form # add form to context dictionary after it is declared
+
+        if self.form.is_valid():
             
             reCAPTCHA_validation_result = validateRecaptcha(request)
             
             if reCAPTCHA_validation_result['success']:
-                from_name = form.cleaned_data['from_name']
-                subject = form.cleaned_data['subject']
-                from_email = form.cleaned_data['from_email']
-                contact_message = form.cleaned_data['contact_message']
-                
+                from_name = self.form.cleaned_data['from_name']
+                subject = self.form.cleaned_data['subject']
+                from_email = self.form.cleaned_data['from_email']
+                contact_message = self.form.cleaned_data['message']
+                '''
                 try:
                     send_mail(subject, contact_message, from_email, ['admin@williamstein.com'])
                 except BadHeaderError:
                     messages.error(request, f'Invalid header found.')
                     return redirect('contact-form')
-                
+                '''
                 messages.success(request, f'Thank you, your message has been sent!')
                 return redirect('contact-form')
             else:
+                print("CAPTCHA Error")
                 messages.error(request, 'Invalid reCAPTCHA. Please try again.')
                 
         else:
-            for error_message in form.errors:
-                messages.error(request, f'{form.errors[error_message]}')
-                
-    else:
-        form = ContactForm()
-        context['form'] = form # add form to context dictionary after it is declared
-    return render(request, 'contact/contact.html', context)
-    
+            for error_message in self.form.errors:
+                messages.error(request, f'{self.form.errors[error_message]}')
+            
+            print("error")
+            for field in self.form:
+                print("Field Error:", field.name,  field.errors)
+            
+            
+        return render(request, self.template, self.context)
 
 def validateRecaptcha(request):
     ''' 
